48f03073a9e153dbb419c82ca352110a2b701495
Enihsuns <enihsunsyxy@gmail.com>
2019-03-29 15:53:59 -0400

    Alter split_compress_image tool: use percentage of image rather than fixed pixel length.

--- tools/split_compress_image.py
+++ tools/split_compress_image.py
@@ -8,10 +8,9 @@ Created on Wed Mar 27 15:09:27 2019
 import os
 from PIL import Image
 
-def split_compress(img_name, in_path, out_path, box_w, box_h, resize_w, resize_h):
+def split_compress(img_name, in_path, out_path, box_factor, resize_w, resize_h):
     '''
-    box_w: Expected image width. 
-    box_h: Expected image height.
+    box_factor: The factor of cropping box corresponding to the original image.
     resize_w: Resized image width.
     resize_h: Resized image height.
     '''
@@ -20,15 +19,25 @@ def split_compress(img_name, in_path, out_path, box_w, box_h, resize_w, resize_h
     
     img_name = img_name.split('.')[:-1]
     
-    row_num, col_num = int(w/box_w), int(h/box_h)
+    box_w = box_h = min(w, h)*box_factor
+    
+    row_num, col_num = 2*int(w/box_w), 2*int(h/box_h)
     
     cnt = 1
     for i in range(row_num):
         for j in range(col_num):
-            cropped_img_name = ''.join(img_name) + '_' + str(box_w) + '_' + str(cnt) + '.jpg'
+            cropped_img_name = ''.join(img_name) + '_' + '{:.0f}'.format(box_w) + '_' + str(cnt) + '.jpg'
             
             # Crop image
-            cropped_img = img.crop(box=(i*box_w, j*box_h, (i + 1)*box_w, (j + 1)*box_h))
+            left, top, right, bottom = i*box_w/2, j*box_h/2, (i/2 + 1)*box_w, (j/2 + 1)*box_h
+            if(right > w):
+                left = left - (right - w)
+                right = w
+            if(bottom > h):
+                top = top - (bottom - h)
+                bottom = h
+            
+            cropped_img = img.crop(box=(left, top, right, bottom))
             
             # Compress image
             cropped_img = cropped_img.resize((resize_w, resize_h))
@@ -38,13 +47,14 @@ def split_compress(img_name, in_path, out_path, box_w, box_h, resize_w, resize_h
             
             cnt = cnt + 1
 
-in_path = '.\\薛亮\\'
-out_path = '.\\data\\generated\\'
+base_path = 'E:\\Work\\ImageGeneration\\'
+in_path = os.path.join(base_path, '白雪石\\') 
+out_path = os.path.join(base_path, 'data\\generated\\')
 
 for item in os.listdir(in_path):
     if(item.split('.')[-1] == 'jpg'):
-        split_compress(item, in_path, out_path, 500, 500, 224, 224)
-        split_compress(item, in_path, out_path, 1000, 1000, 224, 224)
+        split_compress(item, in_path, out_path, 0.3, 500, 500)
+        split_compress(item, in_path, out_path, 1, 500, 500)
     
     
     
